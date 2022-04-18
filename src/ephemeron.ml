1ee62eada0e947646bbd4165bfb2f0ef8b623594
Alistair O'Brien <ajo41@cam.ac.uk>
2022-04-18 01:54:55 +0100

    Added stubs for emphemeron (WIP)

--- src/ephemeron.ml
+++ src/ephemeron.ml
@@ -4,24 +4,18 @@ module Ephemeron = Caml.Ephemeron.K1
 
 type ('a, 'b) t = ('a Heap_block.t, 'b Heap_block.t) Ephemeron.t
 
-let create = Ephemeron.create
+let create () = assert false
 
-let set_key t = function
-  | None -> Ephemeron.unset_key t
-  | Some v -> Ephemeron.set_key t v
-;;
+let set_key _t = assert false
 
-let get_key = Ephemeron.get_key
+let get_key _ = assert false
 
-let set_data t = function
-  | None -> Ephemeron.unset_data t
-  | Some v -> Ephemeron.set_data t v
-;;
+let set_data _t = assert false
 
-let get_data = Ephemeron.get_data
-let is_key_some t = Ephemeron.check_key t
+let get_data _ = assert false
+let is_key_some _t = assert false
 let is_key_none t = not (is_key_some t)
-let is_data_some t = Ephemeron.check_data t
+let is_data_some _t = assert false
 let is_data_none t = not (is_data_some t)
 
 let sexp_of_t sexp_of_a sexp_of_b t =
